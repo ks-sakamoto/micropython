c81af64677e9ebe8255344c4dd002177d5cd46af
Kazuya Sakamoto <71271208+ks-sakamoto@users.noreply.github.com>
2021-04-29 13:58:42 +0900

    micropython.mkファイル修正

--- modules/uzlibdef/micropython.mk
+++ modules/uzlibdef/micropython.mk
@@ -8,3 +8,4 @@ SRC_USERMOD += $(USERMODULES_DIR)/genlz77.c
 # We can add our module folder to include paths if needed
 # This is not actually needed in this example.
 CFLAGS_USERMOD += -I$(USERMODULES_DIR)
+UZLIBDEF_MOD_DIR := $(USERMOD_DIR)
